095e8b6cea8b31926f7b18c5e9b28cae4e2d612b
Marina <zmgmarina@mail.ru>
2022-09-18 23:58:08 +0500

    Add C#

---
+++ C#/02/Program.cs
@@ -0,0 +1,14 @@
+﻿// 
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 == number2 * number2)
+{
+    Console.Write("Первое число является квадратом второго числа");
+}
+else
+{
+  Console.Write("Первое число не является квадратом второго числа");
+}
---
+++ C#/Lesson01/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат
+// (число умноженное само на себя)
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} равен = {sqr}");
+
+
+Console.Write("Введите первое число: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number = int.Parse(Console.ReadLine());
+
+if(number1 == number2*number2)
+{
+    Console.Write("Первое число является квадратом второго числа");
+}
+else
+{
+  Console.Write("Первое число не является квадратом второго числа");
+}
+
