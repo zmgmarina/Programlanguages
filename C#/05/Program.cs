ce15a71a10b1c9a03150334a8292c2cdc17ffb66
Marina <zmgmarina@mail.ru>
2022-09-19 22:57:27 +0500

    Add C#/05

---
+++ C#/05/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напищите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа.
+// 465 -> 6
+
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number >= 100 && number <1000)
+{
+    Console.WriteLine(number % 10);
+}
+else
+{
+    Console.WriteLine("Введено неверное значение");
+}
