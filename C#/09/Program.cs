f13e3bcbeec62bd5f330d9e3e9db03a78154ca64
Marina <zmgmarina@mail.ru>
2022-09-23 01:26:38 +0500

    C#/09/

---
+++ C#/09/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23. 14 -> нет 46 -> нет 161 -> да
+
+int value = Convert.ToInt32(Console.ReadLine());
+
+if (value % 7 == 0 && value % 23 == 0)
+Console.WriteLine($"Число {value} кратно 7 и 23");
+else Console.WriteLine($"Число {value} не кратно 7 и 23");
+
+
+
