66e000ab3e82961d87622b7d6da2f42d718a30b7
Marina <zmgmarina@mail.ru>
2022-09-23 00:24:11 +0500

    C#/07/

---
+++ C#/07/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. 
+// 456 -> 46 782 -> 72 918 -> 98
+int number = new Random().Next(100, 1000); 
+
+int firstDigit = number / 100;
+int thirDigit = number % 10;
+
+Console.WriteLine($"Первая и третья цифра числа {number} - это {firstDigit*10 + thirDigit}");
