fbbdd8bdd800486aa0f2d9a5638cf03fcbe6ce41
Marina <zmgmarina@mail.ru>
2022-09-26 22:21:43 +0500

    C#/Task_23

---
+++ C#/Task_23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите прогр., которая принимает на вход число (N)
+// и  выдает таблицу кубов чисел от 1 до N/
+// 3 -> 1,8,27
+// 5 -> 1,8,27,64,125
+
+int end = Convert.ToInt32(Console.ReadLine());
+
+for(int start = 1; start <= end; start++)
+{
+    Console.WriteLine(start*start*start);
+}
