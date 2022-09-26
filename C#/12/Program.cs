3fd22d9f2605a75d7764a9d045f63a87922ab1ee
Marina <zmgmarina@mail.ru>
2022-09-26 22:18:52 +0500

    C#/12

---
+++ C#/12/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая принимает на вход координаты точки (x, y), причем х ≠ 0 и y ≠ 0
+/* выдает четверть плоскости в которой находится эта точка
+
+ "&&" - и
+ "||" - или
+
+*/
+Console.WriteLine("Введите координату по оси x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату по оси y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0) Console.WriteLine("I четверть");
+else if (x < 0 && y > 0) Console.WriteLine("II четверть"); // else if прим-м, чтобы не проверять все усл, а найти одну истину и заверш проверку 
+else if (x < 0 && y < 0) Console.WriteLine("III четверть");  
+else if (x > 0 && y < 0) Console.WriteLine("IV четверть");  
