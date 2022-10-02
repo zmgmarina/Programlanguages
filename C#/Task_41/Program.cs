a218cb291399cddcbf039d88b19c8f0210e73334
Marina <zmgmarina@mail.ru>
2022-10-02 14:07:17 +0500

    Add ask 41

---
+++ C#/Task_41/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.Write("Введите количество чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[m];
+for (int i = 0; i < m; i++)
+{
+    array[i] = new Random().Next(-10,11);
+    Console.Write(array[i] + "\t ");
+}
+
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+{
+   if(array[i] > 0 )  count += 1; 
+}
+  
+Console.WriteLine();
+Console.WriteLine($"В данном массиве чисел больше 0: {count} ");
